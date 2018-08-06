879f9f7ee15f043fea017b1c55f6bdc9043d33e6
Guionardo Furlan <guionardo@gmail.com>
2018-08-06 16:58:23 -0300

    Primeira implantação com código parcialmente funcional

---
+++ WebSocketServer/http/websocket.js
@@ -0,0 +1,9 @@
+﻿var ws = new WebSocket("ws://localhost:80/chat");
+
+ws.onopen = function (event) {
+    ws.send("Here's some text that the server is urgently awaiting!");
+};
+
+ws.onmessage = function (event) {
+    console.log(event.data);
+}
